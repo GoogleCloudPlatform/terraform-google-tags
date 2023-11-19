55111328b24a04ddd20299adea3aac13da1e491c
Sudharsane Sivamany <89003990+SudharsaneSivamany@users.noreply.github.com>
2023-11-19 23:04:52 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -14,7 +14,52 @@
  * limitations under the License.
  */
 
-output "bucket_name" {
-  description = "Name of the bucket"
-  value       = google_storage_bucket.main.name
+output "key_id" {
+  description = "Key ID"
+  value       = google_tags_tag_key.key.id
+}
+
+output "key_name" {
+  description = "Key Name"
+  value       = google_tags_tag_key.key.name
+}
+
+output "key_namespaced_name" {
+  description = "Key Namespaced Name"
+  value       = google_tags_tag_key.key.namespaced_name
+}
+
+output "value_ids" {
+  description = "List of Value Ids"
+  value       = [for val in var.values : google_tags_tag_value.value[val.value].id]
+}
+
+output "value_names" {
+  description = "List of Value Names"
+  value       = [for val in var.values : google_tags_tag_value.value[val.value].name]
+}
+
+output "value_namespaced_names" {
+  description = "List of Value Namespaced Names"
+  value       = [for val in var.values : google_tags_tag_value.value[val.value].namespaced_name]
+}
+
+output "tag_binding_ids" {
+  description = "List of Tag Binding Ids"
+  value       = distinct(flatten([for val in var.values : [for tag_region, tag_bind in val.tag_binding : tag_region == "global" ? [for bind in tag_bind : google_tags_tag_binding.binding["${val.value}=>${bind}"].id] : []]]))
+}
+
+output "tag_binding_names" {
+  description = "List of Tag Binding Names"
+  value       = distinct(flatten([for val in var.values : [for tag_region, tag_bind in val.tag_binding : tag_region == "global" ? [for bind in tag_bind : google_tags_tag_binding.binding["${val.value}=>${bind}"].name] : []]]))
+}
+
+output "tag_binding_location_ids" {
+  description = "List of Tag Binding Location Ids"
+  value       = distinct(flatten([for val in var.values : [for tag_region, tag_bind in val.tag_binding : tag_region != "global" ? [for bind in tag_bind : google_tags_location_tag_binding.binding["${val.value}=>${tag_region}=>${bind}"].id] : []]]))
+}
+
+output "tag_binding_location_names" {
+  description = "List of Tag Binding Location Names"
+  value       = distinct(flatten([for val in var.values : [for tag_region, tag_bind in val.tag_binding : tag_region != "global" ? [for bind in tag_bind : google_tags_location_tag_binding.binding["${val.value}=>${tag_region}=>${bind}"].name] : []]]))
 }
